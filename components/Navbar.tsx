bab08f1a947ac8caaa163c1e68e1d389d8eea83c
Shabrina Putri <shabbrina5@gmail.com>
2024-01-19 20:01:15 +0700

    add mobile navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { NAV_LINKS } from '@/constants'
 import Button from './Button'
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid"
 
 const Navbar = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+
   return (
     <nav className='max-container padding-container flex justify-between py-8'>
         <div className='left'>
@@ -22,12 +26,39 @@ const Navbar = () => {
                     </Link>
                 ))}
             </ul>
+
+            {/*-------------- NAVBAR LINKS MOBILE -----------------*/}
+            {navbarOpen ? 
+                <ul className='lg:hidden sm:block flex flex-col'>
+                    {NAV_LINKS.map((link) => (
+                        <Link href={link.href} key={link.key} className="flexCenter cursor-pointer pb-2 transition-all hover:font-bold">
+                            {link.label}
+                        </Link>
+                    ))}
+                </ul>
+                : null
+            } 
         </div>
 
         <div className='right lg:flexCenter hidden gap-4'>
             <Button type='button' title='Login' variant='btn_white' />
             <Button type='button' title='Sign Up' variant='btn_purple' />
-        </div>
+        </div>         
+
+        {/*-------------- MENU ICON MOBILE -----------------*/}
+        <div className="block cursor-pointer lg:hidden">
+        {
+            !navbarOpen ? (
+                <button onClick={() => setNavbarOpen(true)}>
+                    <Bars3Icon className='h-5 w-5' />
+                </button>
+            ) : (
+                <button onClick={() => setNavbarOpen(false)}>
+                    <XMarkIcon className='h-5 w-5' />
+                </button>
+            )
+        }
+        </div> 
     </nav>
   )
 }
